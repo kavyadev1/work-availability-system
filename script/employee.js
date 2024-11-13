e6985e1d0ad32e0300f2069540a33c933e72e1ec
kavyadev1 <132856955+kavyadev1@users.noreply.github.com>
2024-11-13 12:22:26 +0000

    commit

---
+++ script/employee.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const employeeButton = document.getElementById("employeeButton");
+
+    employeeButton.addEventListener("click", function () {
+        // Redirect to the employee page
+        window.location.href = "employee.html";
+    });
+});
+
