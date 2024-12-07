f0d26e5dd323a46e8ceaa861aa6d222fa0e573d1
Kavya <kavyadev74@gmail.com>
2024-12-07 13:42:48 +0000

    authcontroller

--- workavailability-server/Controller/authController.js
+++ workavailability-server/Controller/authController.js
@@ -5,3 +5,8 @@ const {generateFromEmail} = require('unique-username-generator')
 
 const StringService = require('../Service/StringService')
 const AuthService = require('../Service/AuthService')
+const MongooseService = require('../Service/MongooseService')
+const UserService = new MongooseService(UserModel);
+const {sendResponse} = require('../Middleware/sendResponse')
+const AggregatorService = require('../Service/AggregatoreService')
+ 
